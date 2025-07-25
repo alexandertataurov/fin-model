8d73fbc5e8ca5967933eec03e066c0c7cdfd99ff
alexandertataurov <alex.tataurov@me.com>
2025-07-25 17:05:04 +0400

    Add user foreign key comment

--- backend/conftest.py
+++ backend/conftest.py
@@ -1,6 +1,8 @@
 import pytest
 import os
 import tempfile
+import warnings
+warnings.filterwarnings("ignore", category=PendingDeprecationWarning)
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
